0eddf7c81115fef4238c88a6f327fb2c62697e9e
Mficius <mayisficius@gmail.com>
2024-06-14 00:40:23 +0100

    last update

--- auth_api/settings.py
+++ auth_api/settings.py
@@ -40,6 +40,14 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(",")
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8080']
+
+
+
 
 # Application definition
 
